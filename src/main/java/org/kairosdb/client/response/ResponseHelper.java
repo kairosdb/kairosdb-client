6587f2b614b8445398c48e915e5796b62cdf64dc
Brian Hawkins <brianhks1@gmail.com>
2025-11-12 16:17:22 -0700

    Updated dependencies to address vulnerabilities

--- src/main/java/org/kairosdb/client/response/ResponseHelper.java
+++ src/main/java/org/kairosdb/client/response/ResponseHelper.java
@@ -1,6 +1,6 @@
 package org.kairosdb.client.response;
 
-import org.apache.commons.lang.NotImplementedException;
+import org.apache.commons.lang3.NotImplementedException;
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 
